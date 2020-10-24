3064f3e20bafc1cbf2b87617258d8f7784fa5e14
Cyclic3 <cyclic3.git@gmail.com>
2020-10-24 14:07:25 +0100

    Making join/leave more tidy

--- app/client.py
+++ app/client.py
@@ -2,5 +2,6 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix='$')
 
+
 bot_role_id = 769199572399423488
 course_reps_role_id = 768926882888155196
--- app/open.py
+++ app/open.py
@@ -9,10 +9,12 @@ class OpenCommands(commands.Cog, name="Commands anyone can use"):
     @commands.command(help="Adds you to the group for the give modules")
     async def join(self, ctx: commands.Context, *args):
         await asyncio.gather(*[module.add_student(ctx, ctx.author, i.upper()) for i in args])
+        await ctx.message.delete()
 
     @commands.command(help="Removes you from the group for the give modules")
     async def leave(self, ctx: commands.Context, *args):
         await asyncio.gather(*[module.remove_student(ctx, ctx.author, i.upper()) for i in args])
+        await ctx.message.delete()
 
     @commands.command(help="Renders a LaTeX equation")
     async def maths(self, ctx, *args):
